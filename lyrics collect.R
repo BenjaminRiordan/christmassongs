eb713905840725ac5a8e332cc94701ee8ff38c1f
Benjamin Riordan <BRiordan@ltu.edu.au>
2022-07-08 11:20:30 +1000

    initial commit

--- lyrics collect.R
+++ lyrics collect.R
@@ -1,2 +1,13 @@
 ## Pull Wikipedia table - list of Christmas songs
 
+# Packages
+library (rvest)
+library (tidyverse)
+
+# Scrape christmas list
+
+url <- "https://en.wikipedia.org/wiki/List_of_popular_Christmas_singles_in_the_United_States"
+
+christmas_US <- url %>% read_html() %>% 
+  html_table(header = TRUE, fill = TRUE)  %>% 
+  .[[3]]
